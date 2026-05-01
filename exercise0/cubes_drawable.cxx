cc5d76355690d6997afe9b6e77d0d200bdace320
hyson <hysonplato@outlook.com>
2026-05-01 18:18:25 +0200

    task 0.2a

--- exercise0/cubes_drawable.cxx
+++ exercise0/cubes_drawable.cxx
@@ -26,6 +26,36 @@
 // ************************************************************************************/
 // Task 0.2a: Create a drawable that provides a (for now, empty) GUI and supports
 //            reflection, so that its properties can be set via config file.
+class cubes_fractal_render : //like the demo create a herientance structure which has the function in reflection and providing gui and can be rendered
+	public cgv::base::base,      // This class supports reflection
+	public cgv::gui::provider,   // Instances of this class provde a GUI
+	public cgv::render::drawable // Instances of this class can be rendered
+{
+protected:
+
+
+public:
+	std::string get_type_name(void) const {
+
+		return "cubes_fractal";
+	}
+	bool self_reflect(cgv::reflect::reflection_handler& rh) {
+	
+		return rh.reflect_member();
+	}
+	void create_gui(void) {
+	
+	}
+
+	void draw(cgv::render::context& ctx) {
+	
+	}
+};
+
+cgv::base::object_registration<cubes_fractal_render> cubes_fractal_render_registration(
+	"cubes_fractal_render"
+);
+
 //
 // Task 0.2b: Utilize the cubes_fractal class to render a fractal of hierarchically
 //            transformed cubes. Expose its recursion depth and color properties to GUI
